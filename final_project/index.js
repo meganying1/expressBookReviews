9e97c7bcec8da20b9ba415ec1cf52f042ceb9e0f
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-07-03 15:31:34 -0700

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -10,9 +10,29 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/user", (req, res, next) => {
+    // check if user is authenticated
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
+        
+        // verify JWT token for user authentication
+        jwt.verify(token, "access", (err, user) => {
+            // set authenticated user data on the request object and proceed to next middleware
+            if (!err) {
+                req.user = user;
+                next();
+            // return error if token verification fails
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+        
+    // return error if no access token is found in the session
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
 });
+
  
 const PORT =5000;
 
